f202696c9cf7a8c3febfc5adb5348923482ee295
lisefozing <fozinglise@gmail.com>
2026-04-29 17:26:01 +0100

    Initialisation complète de l'architecture microservices

---
+++ services/auth-service/src/test/java/com/ziaant/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziaant.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
